2e15cbf3f8c3ac783836e4211b680e6c70370b87
XXhaos <53092039+XXhaos@users.noreply.github.com>
2025-08-15 15:43:35 +0800

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,3 @@
+python main.py --compressor "Lossless" --input_path "input/SRR554369.fastq" --output_path "output" --mode "c"
+#python main.py --compressor "Lossy" --input_path "/media/compress/新加卷1/output/Test/test_FastCA-Lossy4.8/SRR14626645_1" --output_path "FastqCA_Lossy" --mode "d"
+#python main.py --compressor "Lossy" --input_path "output/SRR554369" --output_path "output" --mode "d"
